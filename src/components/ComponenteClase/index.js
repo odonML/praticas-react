98119ddcbf027915b83f24e1439a5acbf54aec0b
odonML <odon_ml@outlook.com>
2021-12-17 18:21:56 -0600

    tipos de props y props tipados

--- src/components/ComponenteClase/index.js
+++ src/components/ComponenteClase/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+//Este es un componente de clase en react
+//el cual recibe un prop msj
 class ComponenteClase extends Component {
     render(){
         return <h1>{this.props.msj}</h1>
