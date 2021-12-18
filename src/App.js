45e6f94e5f771b45d8d45b0ca17d7252f2eb54fc
odonML <odon_ml@outlook.com>
2021-12-17 18:43:53 -0600

    estado en componente de clase

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
 import ComponenteClase from "./components/ComponenteClase/index";
 import ComponenteFuncional from "./components/ComponenteFuncional";
+import EstadoClase from "./components/EstadoClase";
 import Propiedades from "./components/Propiedades";
 
 function App() {
     return (
         <div className="App">
-            <ComponenteClase msj="hola soy un componente de clase" />
+            {/* <ComponenteClase msj="hola soy un componente de clase" />
             <ComponenteFuncional msj="hola soy un componente funcional" />
             <Propiedades
                 cadena="soy texto"
@@ -17,7 +18,8 @@ function App() {
                 funcion={(num) => num * num}
                 elementoReact={<h1>soy un elemento react</h1>}
                 componente={<ComponenteFuncional msj="hola"/>}
-            />
+            /> */}
+            <EstadoClase />
         </div>
     );
 }
